bc08c796dba6f4f509ffb9f748d76cc7bff0c899
gege114514 <3655017035@qq.com>
2026-07-21 15:35:40 +0800

    Update OnlineServerEntryMixin.java

--- src/main/java/com/example/fixedserverlist/mixin/OnlineServerEntryMixin.java
+++ src/main/java/com/example/fixedserverlist/mixin/OnlineServerEntryMixin.java
@@ -4,7 +4,7 @@ import com.example.fixedserverlist.FixedServerConfig;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
-import net.minecraft.client.server.ServerData;
+import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
