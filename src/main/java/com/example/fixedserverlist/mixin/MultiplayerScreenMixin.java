0eea17a7d0f61868b17314840bc13f2a1adaf267
gege114514 <3655017035@qq.com>
2026-07-21 15:33:43 +0800

    Update MultiplayerScreenMixin.java

--- src/main/java/com/example/fixedserverlist/mixin/MultiplayerScreenMixin.java
+++ src/main/java/com/example/fixedserverlist/mixin/MultiplayerScreenMixin.java
@@ -2,7 +2,7 @@ package com.example.fixedserverlist.mixin;
 
 import net.minecraft.client.gui.screens.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
-import net.minecraft.client.server.ServerData;
+import net.minecraft.client.multiplayer.ServerData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
